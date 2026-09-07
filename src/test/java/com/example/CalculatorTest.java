0bacd4636a43e5b4d37aee7a9f3e389cc10b3c88
Gulzar Patel <patelgulzar01@gmail.com>
2026-09-07 06:59:56 +0530

    Fixed Code

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -8,10 +8,8 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        assertEquals(5, calc.add(2, 3));  // Change this line to:
-        assertEquals(6, calc.add(2, 3));  // Make it wrong
+        assertEquals(5, calc.add(2, 3));
     }
-
 
     @Test
     public void testSubtract() {
